d017d65f6ff79bc6123d0e9a1842bca6ac98e870
Uriel Silva <urielbcs@gmail.com>
2015-08-31 11:22:48 -0300

    fixing config issues

--- config/initializers/redis.rb
+++
@@ -1,4 +0,0 @@
-if Rails.env.production? or Rails.env.staging?
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-end
